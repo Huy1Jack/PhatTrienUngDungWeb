2b167136c6d01175395b404d88cde8113e7ef39e
HoangHuy <hoanghuya1thptcc@gmail.com>
2024-10-18 18:21:43 +0700

    fpoly

---
+++ wooden/wooden/Models/WoodContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace wooden.Models;
+
+public partial class WoodContext : DbContext
+{
+    public WoodContext()
+    {
+    }
+
+    public WoodContext(DbContextOptions<WoodContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TbProduct> TbProducts { get; set; }
+
+    public virtual DbSet<TbProductCategory> TbProductCategories { get; set; }
+
+    public virtual DbSet<TbProductReview> TbProductReviews { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source= DESKTOP-VLQM3MM; initial catalog=wood; integrated security=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TbProduct>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__tb_Produ__B40CC6CD1256A44A");
+
+            entity.ToTable("tb_Product");
+
+            entity.Property(e => e.Alias).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Image).HasMaxLength(255);
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.PriceSale).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Star).HasColumnType("decimal(3, 2)");
+            entity.Property(e => e.Title).HasMaxLength(255);
+
+            entity.HasOne(d => d.CategoryProduct).WithMany(p => p.TbProducts)
+                .HasForeignKey(d => d.CategoryProductId)
+                .HasConstraintName("FK__tb_Produc__Categ__440B1D61");
+        });
+
+        modelBuilder.Entity<TbProductCategory>(entity =>
+        {
+            entity.HasKey(e => e.CategoryProductId).HasName("PK__tb_Produ__FAFA184FC598CEBA");
+
+            entity.ToTable("tb_ProductCategory");
+
+            entity.Property(e => e.Alias).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Icon).HasMaxLength(255);
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<TbProductReview>(entity =>
+        {
+            entity.HasKey(e => e.ProductReviewId).HasName("PK__tb_Produ__396318807C05C4A4");
+
+            entity.ToTable("tb_ProductReview");
+
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.Star).HasColumnType("decimal(3, 2)");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.TbProductReviews)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__tb_Produc__Produ__46E78A0C");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
